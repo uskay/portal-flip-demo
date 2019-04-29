4be31a2015f22795b1755e4ec539b1e5f98a5d42
uskay <uskay@google.com>
2019-04-29 21:32:18 +0900

    Changed portals-controller.js to be a standalone script

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ $ git clone --depth 1 https://github.com/uskay/portal-flip-demo.git
 $ cd portal-flip-demo
 $ npm install
 $ npm run demo
+```
 
-Access [http://localhost:8080/](http://localhost:8080/)
+Access [http://localhost:8080/fuji.html](http://localhost:8080/fuji.html)
+
+## Explainer
+
+TODO: Write explainer
+
--- js/portals-controller.js
+++ js/portals-controller.js
@@ -8,8 +8,16 @@ class PortalController {
      * Constructor
      * @param {Object} option 
      */
-    constructor(option) {
-        this.option = option;
+    constructor(pageList) {
+        this.option = {
+            pageList: pageList,
+            upperFrontSelector: "#upperFold .front",
+            upperBackSelector: "#upperFold .back",
+            lowerFrontSelector: "#lowerFold .front",
+            lowerBackSelector: "#lowerFold .back",
+            nextPortalSelector: "#nextPortal",
+            prevPortalSelector: "#prevPortal"
+        };
     }
 
     /**
@@ -17,7 +25,6 @@ class PortalController {
      */
     init() {
         const {
-            currentPageIndex, 
             pageList, 
             upperFrontSelector, 
             upperBackSelector, 
@@ -27,8 +34,10 @@ class PortalController {
             prevPortalSelector
         } = this.option;
 
-        // Save page related values
-        this.currentPageIndex = currentPageIndex;
+        // Append flip elements
+        this._createFlip();
+
+        // Save page list
         this.pageList = pageList;
 
         // Init flags
@@ -36,7 +45,7 @@ class PortalController {
         this.initDone = false;
 
         // Add contents to the upper front div and lower front div as Shadow DOM
-        this._appendShadowDom([upperFrontSelector, lowerFrontSelector], pageList[currentPageIndex]);
+        this._appendShadowDom([upperFrontSelector, lowerFrontSelector], location.pathname);
 
         // If it's the inital page load
         if(!window.portalHost) {
@@ -66,20 +75,38 @@ class PortalController {
             prevPortalSelector
         } = this.option;
         if(this._hasNextPage()) {
-            const nextPortal = this._createPortal(`${location.origin + location.pathname}?page=${currentPageIndex+1}`);
+            const nextPortal = this._createPortal(this.pageList[location.pathname].next);
             const lowerBackPortal = nextPortal.cloneNode();
             this._appendPortal(nextPortalSelector, nextPortal);
             this._appendPortal(lowerBackSelector, lowerBackPortal);
         }
         if(this._hasPrevPage()){
             const prevPortal = predecessor ? predecessor :
-                this._createPortal(`${location.origin + location.pathname}?page=${currentPageIndex-1}`);
+                this._createPortal(this.pageList[location.pathname].prev);
             const upperBackPortal = prevPortal.cloneNode(upperBackSelector);
             this._appendPortal(prevPortalSelector, prevPortal);
             this._appendPortal(upperBackSelector, upperBackPortal);
         }
         this._hookEvents();
         this.initDone = true;
+    }
+
+    _createFlip() {
+        const div = document.createElement('div');
+        const flipElements = `
+            <div id="upperFold">
+                <div class="front"></div>
+                <div class="back"></div>
+            </div>
+            <div id="lowerFold">
+                <div class="front"></div>
+                <div class="back"></div>
+            </div>
+            <div id="nextPortal"></div>
+            <div id="prevPortal"></div>
+        `
+        div.innerHTML = flipElements;
+        document.body.appendChild(div);
     }
 
     /**
@@ -131,7 +158,7 @@ class PortalController {
      * @returns {boolean}
      */
     _hasNextPage() {
-        return this.pageList.length > this.currentPageIndex + 1;
+        return this.pageList[location.pathname].next !== undefined;
     }
 
     /**
@@ -139,7 +166,7 @@ class PortalController {
      * @returns {boolean}
      */
     _hasPrevPage() {
-        return this.currentPageIndex > 0;
+        return this.pageList[location.pathname].prev !== undefined;
     }
 
     /**
@@ -160,15 +187,20 @@ class PortalController {
             }
             this.transitionSelector = selector;
             this.isTransitioning = true;
-            document.querySelector(selector).parentNode.style.zIndex = 1;
             document.querySelector(selector).parentNode.style.transition = "all 0.5s ease-in-out";
             document.querySelector(selector).parentNode.style.transform = "rotate3d(1, 0, 0, 180deg)";
             if(selector === lowerFrontSelector) {
+                document.querySelector(upperFrontSelector).parentNode.style.zIndex = 1;
+                document.querySelector(lowerFrontSelector).parentNode.style.zIndex = 2;
+                document.querySelector(nextPortalSelector).style.zIndex = 0;
                 document.querySelector(nextPortalSelector).style.opacity = 1;
                 document.querySelector(prevPortalSelector).style.opacity = 0;
                 this.activatePortal = document.querySelector(`${nextPortalSelector} portal`);
                 return;
             }
+            document.querySelector(upperFrontSelector).parentNode.style.zIndex = 2;
+            document.querySelector(lowerFrontSelector).parentNode.style.zIndex = 1;
+            document.querySelector(prevPortalSelector).style.zIndex = 0;
             document.querySelector(nextPortalSelector).style.opacity = 0;
             document.querySelector(prevPortalSelector).style.opacity = 1;
             this.activatePortal = document.querySelector(`${prevPortalSelector} portal`);
@@ -204,36 +236,22 @@ class PortalController {
     }
 }
 
-// Retrieving the current page index from the query parameter 
-const getCurrentPageIndex = _ => {
-    const matchResult = location.search.match(/\?page=(\d)/);
-    if(matchResult && matchResult[1]) {
-       return parseInt(matchResult[1]); 
-    } else {
-       return 0;
-    }
-}
-const currentPageIndex = getCurrentPageIndex();
-
 // The list of pages to flip
-const pageList = [
-    '/view/red.html',
-    '/view/blue.html',
-    '/view/yellow.html'
-]
-
-// Init options
-const options = {
-    currentPageIndex: currentPageIndex,
-    pageList: pageList,
-    upperFrontSelector: "#upperFold .front",
-    upperBackSelector: "#upperFold .back",
-    lowerFrontSelector: "#lowerFold .front",
-    lowerBackSelector: "#lowerFold .back",
-    nextPortalSelector: "#nextPortal",
-    prevPortalSelector: "#prevPortal"
+const pageList = {
+    '/fuji.html': {
+        prev: '/tokyotower.html',
+        next: '/okinawa.html'
+    },
+    '/okinawa.html': {
+        prev: '/fuji.html',
+        next: '/tokyotower.html'
+    },
+    '/tokyotower.html': {
+        prev: '/okinawa.html',
+        next: '/fuji.html'
+    },
 }
 
 // Start flippping ;-)
-const controller = new PortalController(options);
+const controller = new PortalController(pageList);
 controller.init();
